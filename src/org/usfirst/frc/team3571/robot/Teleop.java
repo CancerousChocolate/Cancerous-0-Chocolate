c7075cfb4dabc0d3db5e52089a8e6b3c7b122cfe
TomasR <Rakusan2@gmail.com>
2016-11-10 20:52:08 -0500

    mess none

--- src/org/usfirst/frc/team3571/robot/Teleop.java
+++ src/org/usfirst/frc/team3571/robot/Teleop.java
@@ -32,7 +32,7 @@ public class Teleop extends OI {
 		// TODO Make Teleop Code
 		
 		if(test){
-			System.out.print("hello");
+			System.out.print("none");
 		}
 		
 		// Flips the state of triggerDrive when Button B changes state to
